9a8d10796d9dcd96c2574c49b5b7a1befff3cba9
robwelan <robwelan@yahoo.com.au>
2018-09-24 15:12:48 +1000

    Expand required framework assets

--- src/frameworks/semantic-ui-react.js
+++ src/frameworks/semantic-ui-react.js
@@ -10,6 +10,7 @@ import Menu from 'semantic-ui-react/dist/commonjs//collections/Menu';
 // Elements
 import Button from 'semantic-ui-react/dist/commonjs/elements/Button/Button';
 import Container from 'semantic-ui-react/dist/commonjs/elements/Container';
+import Header from 'semantic-ui-react/dist/commonjs/elements/Header';
 import Icon from 'semantic-ui-react/dist/commonjs/elements/Icon'
 import Image from 'semantic-ui-react/dist/commonjs/elements/Image';
 import List from 'semantic-ui-react/dist/commonjs/elements/List'
@@ -19,6 +20,9 @@ import Segment from 'semantic-ui-react/dist/commonjs/elements/Segment';
 import Grid from 'semantic-ui-react/dist/commonjs/collections/Grid';
 import GridColumn from 'semantic-ui-react/dist/commonjs/collections/Grid/GridColumn';
 import GridRow from 'semantic-ui-react/dist/commonjs/collections/Grid/GridRow';
+
+// Modules
+import Dropdown from 'semantic-ui-react/dist/commonjs/modules/Dropdown';
 
 // Views
 import Item from 'semantic-ui-react/dist/commonjs/views/Item'
@@ -33,11 +37,13 @@ import Item from 'semantic-ui-react/dist/commonjs/views/Item'
 export {
   Button,
   Container,
+  Dropdown,
   Form,
   FormField,
   Grid,
   GridColumn,
   GridRow,
+  Header,
   Icon,
   Item,
   Image,
