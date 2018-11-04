31658d37431627d6ce105302ee143262352beb22
robwelan <robwelan@yahoo.com.au>
2018-11-04 13:57:05 +1000

    End of section 17

--- src/features/user/Settings/PhotosPage.jsx
+++ src/features/user/Settings/PhotosPage.jsx
@@ -1,11 +1,269 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import Dropzone from 'react-dropzone';
+import Cropper from 'react-cropper';
+import { toastr } from 'react-redux-toastr';
+import {
+  Button,
+  Card,
+  Divider,
+  Icon,
+  Image,
+  Grid,
+  Header,
+  Segment,
+} from '../../../frameworks/semantic-ui-react/scripts';
+import {
+  deletePhoto,
+  setMainPhoto,
+  uploadProfileImage,
+} from '../userActions';
+import '../../../../node_modules/cropperjs/dist/cropper.css';
 
-const PhotosPage = () => {
-  return (
-    <div>
-      <h1>Photos Page</h1>
-    </div>
-  );
+const actions = {
+  doDeletePhoto: deletePhoto,
+  doSetMainPhoto: setMainPhoto,
+  doUploadProfileImage: uploadProfileImage,
 };
 
-export default PhotosPage;
+const mapState = state => ({
+  auth: state.firebase.auth,
+  loading: state.async.loading,
+  photos: state.firestore.ordered.photos,
+  profile: state.firebase.profile,
+});
+
+const query = ({ auth }) => [
+  {
+    collection: 'users',
+    doc: auth.uid,
+    subcollections: [
+      {
+        collection: 'photos',
+      },
+    ],
+    storeAs: 'photos',
+  },
+];
+
+class PhotosPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      files: [],
+      fileName: '',
+      cropResult: null,
+      image: {},
+    };
+
+    this.cancelCrop = this.cancelCrop.bind(this);
+    this.cropImage = this.cropImage.bind(this);
+    this.handlePhotoDelete = this.handlePhotoDelete.bind(this);
+    this.onDrop = this.onDrop.bind(this);
+    this.uploadImage = this.uploadImage.bind(this);
+  }
+
+  onDrop(files) {
+    this.setState({
+      files,
+      fileName: files[0].name,
+    });
+  }
+
+  cropImage() {
+    if (typeof this.refs.cropper.getCroppedCanvas() === 'undefined') {
+      return;
+    }
+
+    this.refs.cropper.getCroppedCanvas().toBlob((blob) => {
+      const imageUrl = URL.createObjectURL(blob);
+
+      this.setState({
+        cropResult: imageUrl,
+        image: blob,
+      });
+    }, 'image/jpeg');
+  }
+
+  cancelCrop() {
+    this.setState({
+      files: [],
+      image: {},
+    });
+  }
+
+  handlePhotoDelete(photo) {
+    const { doDeletePhoto } = this.props;
+
+    return async () => {
+      try {
+        doDeletePhoto(photo);
+      } catch (error) {
+        toastr.error('Oops', error.message);
+      }
+    };
+  }
+
+  handleSetMainPhoto(photo) {
+    const { doSetMainPhoto } = this.props;
+
+    return async () => {
+      try {
+        doSetMainPhoto(photo);
+      } catch (error) {
+        toastr.error('Oops', error.message);
+      }
+    };
+  }
+
+  async uploadImage() {
+    try {
+      const { doUploadProfileImage } = this.props;
+      const { image, fileName } = this.state;
+
+      await doUploadProfileImage(image, fileName);
+      this.cancelCrop();
+
+      toastr.success('Success!', 'Photo has been uploaded.');
+    } catch (error) {
+      toastr.error('Oops', error.message);
+    }
+  }
+
+  render() {
+    const {
+      cropResult,
+      files,
+    } = this.state;
+    const {
+      loading,
+      photos,
+      profile,
+    } = this.props;
+
+    let filteredPhotos;
+    if (photos) {
+      filteredPhotos = photos.filter(photo => (
+        photo.url !== profile.photoURL
+      ));
+    }
+
+    return (
+      <Segment>
+        <Header dividing size="large" content="Your Photos" />
+        <Grid>
+          <Grid.Row />
+          <Grid.Column width={4}>
+            <Header color="teal" sub content="Step 1 - Add Photo" />
+            <Dropzone
+              multiple={false}
+              onDrop={this.onDrop}
+            >
+              <div style={{ padding: '30px', textAlign: 'center' }}>
+                <Icon name="upload" size="huge" />
+                <Header content="Drop image here or click to add." />
+              </div>
+            </Dropzone>
+          </Grid.Column>
+          <Grid.Column width={1} />
+          <Grid.Column width={4}>
+            <Header sub color="teal" content="Step 2 - Resize image" />
+            {files[0]
+              && (
+                <Cropper
+                  aspectRatio={1}
+                  crop={this.cropImage}
+                  cropBoxMovable={true}
+                  cropBoxResizable={true}
+                  dragMode="move"
+                  guides={false}
+                  ref="cropper"
+                  scalable={true}
+                  src={files[0].preview}
+                  style={{ height: 200, width: '100%' }}
+                  viewMode={0}
+                />
+              )
+            }
+          </Grid.Column>
+          <Grid.Column width={1} />
+          <Grid.Column width={4}>
+            <Header sub color="teal" content="Step 3 - Preview and Upload" />
+            {files[0]
+              && (
+                <div>
+                  <Image
+                    src={cropResult}
+                    style={{ minHeight: '200px', minWidth: '200px' }}
+                  />
+                  <Button.Group>
+                    <Button
+                      icon="check"
+                      loading={loading}
+                      onClick={this.uploadImage}
+                      positive
+                      style={{ width: '100px' }}
+                    />
+                    <Button
+                      disabled={loading}
+                      icon="close"
+                      onClick={this.cancelCrop}
+                      style={{ width: '100px' }}
+                    />
+                  </Button.Group>
+                </div>
+              )
+            }
+          </Grid.Column>
+
+        </Grid>
+
+        <Divider />
+        <Header sub color="teal" content="All Photos" />
+
+        <Card.Group itemsPerRow={5}>
+          <Card>
+            <Image src={profile.photoURL || '/assets/user.png'} />
+            <Button positive>Main Photo</Button>
+          </Card>
+
+          {
+            photos
+            && (
+              filteredPhotos.map(photo => (
+                <Card key={photo.id}>
+                  <Image
+                    src={photo.url}
+                  />
+                  <div className="ui two buttons">
+                    <Button
+                      basic
+                      color="green"
+                      onClick={this.handleSetMainPhoto(photo)}
+                    >
+                      Main
+                    </Button>
+                    <Button
+                      basic
+                      icon="trash"
+                      color="red"
+                      onClick={this.handlePhotoDelete(photo)}
+                    />
+                  </div>
+                </Card>
+              ))
+            )
+          }
+        </Card.Group>
+      </Segment>
+    );
+  }
+}
+
+export default compose(
+  connect(mapState, actions),
+  firestoreConnect(auth => query(auth)),
+)(PhotosPage);
--- src/features/user/userActions.js
+++ src/features/user/userActions.js
@@ -1,5 +1,11 @@
 import moment from 'moment';
+import cuid from 'cuid';
 import { toastr } from 'react-redux-toastr';
+import {
+  asyncActionError,
+  asyncActionFinish,
+  asyncActionStart,
+} from '../async/asyncActions';
 
 const isValidDate = (date) => {
   const d = moment(date);
@@ -31,7 +37,7 @@ export const updateProfile = user => (
 
     if (userBirthDate !== storeBirthDate) {
       const newBirthDate = moment(userBirthDate).toDate();
-      console.log(newBirthDate)
+
       if (typeof (storeBirthDate) === 'undefined') {
         // birth date has never been entered prior to this...
         updatedUser.dateOfBirth = newBirthDate;
@@ -53,3 +59,98 @@ export const updateProfile = user => (
       console.error('Error Report: ', { error });
     }
   });
+
+export const uploadProfileImage = (file, fileName) => (
+  async (dispatch, getState, { getFirebase, getFirestore }) => {
+    const imageName = cuid();
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+    const user = firebase.auth().currentUser;
+    const path = `${user.uid}/user_images`;
+    const options = {
+      name: imageName,
+    };
+
+    try {
+      dispatch(asyncActionStart());
+      // upload file to firebase storage
+      const uploadedFile = await firebase.uploadFile(path, file, null, options);
+      // get url of image
+      const downloadURL = await uploadedFile.uploadTaskSnapshot.ref.getDownloadURL();
+      // get userdoc from firestore
+      const userDoc = await firestore.get(`users/${user.uid}`);
+      // check if user has photo, if not, update profile with new image
+      if (!userDoc.data().photoURL) {
+        await firebase.updateProfile({
+          photoURL: downloadURL,
+        });
+        await user.updateProfile({
+          photoURL: downloadURL,
+        });
+      }
+
+      // add the new photo to photos collection
+      await firestore.add(
+        {
+          collection: 'users',
+          doc: user.uid,
+          subcollections: [{ collection: 'photos' }],
+        },
+        {
+          name: imageName,
+          url: downloadURL,
+        },
+      );
+
+      dispatch(asyncActionFinish());
+    } catch (error) {
+      console.log(error);
+      dispatch(asyncActionError());
+      throw new Error('Problem uploading photo');
+    }
+  }
+);
+
+export const deletePhoto = photo => (
+  async (dispatch, getState, { getFirebase, getFirestore }) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+    const user = firebase.auth().currentUser;
+
+    try {
+      await firebase.deleteFile(`${user.uid}/user_images/${photo.name}`);
+      await firestore.delete(
+        {
+          collection: 'users',
+          doc: user.uid,
+          subcollections: [
+            {
+              collection: 'photos',
+              doc: photo.id,
+            },
+          ],
+        },
+      );
+    } catch (error) {
+      console.log(error);
+      throw new Error('Problem deleting the photo');
+    }
+  }
+);
+
+export const setMainPhoto = photo => (
+  async (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+
+    try {
+      return await firebase.updateProfile(
+        {
+          photoURL: photo.url,
+        },
+      );
+    } catch (error) {
+      console.log(error);
+      throw new Error('Problem setting main photo');
+    }
+  }
+);
